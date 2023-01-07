f2419438469794cfdda681413fcbf5cecf0d6dc4
pklacher <pia.lacher04@htl-salzburg.ac.at>
2023-01-07 14:29:01 +0100

    add rotation and  graphics

--- Assets/MyGame/Scripts/PlayerManager.cs
+++ Assets/MyGame/Scripts/PlayerManager.cs
@@ -24,16 +24,23 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
+        //float horizontalMovement = Input.GetAxis("Horizontal");
+        /*float verticalMovement = Input.GetAxis("Vertical");
 
-        playerMovement = new Vector2(horizontalMovement, verticalMovement).normalized;
+        playerMovement = new Vector2(playerRB.transform.position.x, verticalMovement).normalized;*/
+
     }
 
     private void FixedUpdate()
     {
-        playerRB.velocity = new Vector2(playerMovement.x, playerMovement.y) * normalSpeed;
-        
+        /*playerRB.velocity = new Vector2(playerRB.transform.position.x, playerMovement.y) * normalSpeed;
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * (normalSpeed / 2));*/
+
+        float movement = Input.GetAxis("Vertical") * normalSpeed;
+        float rotation = Input.GetAxis("Horizontal") * normalSpeed / 2;
+
+        playerRB.velocity = transform.up * movement;
+        playerRB.SetRotation(playerRB.rotation - rotation);
     }
 
 
